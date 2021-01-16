9b976af46f79dc06ac5e67176d3c7ae9f3646a49
unrccc2 <tylee.l.kareck@gmail.com>
2021-01-15 16:41:04 -0800

    Commented wobble goal case directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UNR_CCC_UG_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UNR_CCC_UG_Auto.java
@@ -36,10 +36,13 @@ public class UNR_CCC_UG_Auto extends LinearOpMode {
 
         switch (stack) {
             case ZERO:
+                // move the front of the robot 4 tiles forward and 1/2 tile to the right (if on red, left on blue)
                 break;
             case ONE:
+                // move the front of the robot 5 tiles forward and 1/2 a tile to the left (right on blue)
                 break;
             case FOUR:
+                // move the front of the robot 6 tiles forward and 1/2 a tile to the right (left on blue)
                 break;
             default:
                 break;
