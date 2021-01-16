b36ce934dc821a3b95de39f9b9db4c059a7afb96
unrccc2 <tylee.l.kareck@gmail.com>
2021-01-15 16:21:39 -0800

    Initializes camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UNR_CCC_UG_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UNR_CCC_UG_Auto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.vision.UGRectDetector;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -14,10 +15,15 @@ public class UNR_CCC_UG_Auto extends LinearOpMode {
     HardwarePushbot robot   = new HardwarePushbot();   // Use a Pushbot's hardware
     private ElapsedTime runtime = new ElapsedTime();
 
+//        initializing stack detector
+    com.arcrobotics.ftclib.vision.UGRectDetector UGRectDetector = new UGRectDetector(hardwareMap);
+
     @Override
     public void runOpMode() {
 
         robot.init(hardwareMap);
+
+        UGRectDetector.init();
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
@@ -25,6 +31,19 @@ public class UNR_CCC_UG_Auto extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-    }
 
+        UGRectDetector.Stack stack = UGRectDetector.getStack();
+
+        switch (stack) {
+            case ZERO:
+                break;
+            case ONE:
+                break;
+            case FOUR:
+                break;
+            default:
+                break;
+
+        }
+    }
 }
